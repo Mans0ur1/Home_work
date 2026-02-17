e532388a56da1d915eb5f4f65e40cd7acb278578
mansour <mm9256127@gmail.com>
2026-02-17 19:22:38 +0200

    linked_list

--- sec9_home_work/q3.dart
+++ sec9_home_work/q3.dart
@@ -1,20 +1,23 @@
 int search(List<int> nums, int target) {
-  int index = -1;
-  for (int i = 0; i < nums.length; i++) {
-    if (nums[i] == target) {
-      index = i;
-     break;
+  int left = 0;
+  int right = nums.length - 1;
+
+  while (left <= right) {
+    int mid = (left + right) ~/ 2;
+
+    if (nums[mid] == target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
-    ;
   }
-  return index;
+
+  return -1;
 }
 
 void main() {
-  List<int> nums1 = [-1, 0, 3, 5, 9, 12];
-  int target1 = 9;
-  print(search(nums1, target1));
-  List<int> nums2 = [-1, 0, 3, 5, 9, 12];
-  int target2 = 2;
-  print(search(nums2, target2));
+  print(search([-1, 0, 3, 5, 9, 12], 9));  // 4
+  print(search([-1, 0, 3, 5, 9, 12], 2));  // -1
 }
